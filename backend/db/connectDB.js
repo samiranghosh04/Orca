0ec79fe1bcb3f46486e5b758b45e1a031badb17d
Rudrava Mukherjee <rudravamukherjee.2001@gmail.com>
2024-01-19 21:46:08 +0530

    fix: routing and add logging

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
+        if (conn.connection) console.log("connected to mongodb")
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,31 @@ import connectDB from './db/connectDB.js';
 import cookieParser from 'cookie-parser';
 
 import userRoutes from "./routes/userRoutes.js";
+import morgan from 'morgan';
 
 dotenv.config();
-connectDB();
+await connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(morgan("dev"))
 app.use(express.json()); // To parse JSON data in the req.body
-app.use(express.urlencoded({extended: true})); // To parse form data into the req body
+app.use(express.urlencoded({ extended: true })); // To parse form data into the req body
 app.use(cookieParser());
 
 //Routes
-app.use("api/users",userRoutes);
+app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
+
+process.on('SIGTERM', () => {
+    console.info('SIGTERM signal received.');
+    console.log('Closing http server.');
+    server.close((err) => {
+        console.log('Http server closed.');
+        process.exit(err ? 1 : 0);
+    });
+});
+
 
